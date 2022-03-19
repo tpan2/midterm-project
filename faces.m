7f93254efd0072daa0ff8d65025c918b86e8d195
tpan2 <101124218+tpan2@users.noreply.github.com>
2022-03-19 18:31:03 -0400

    Update faces.m
    Update to pull image from url

--- faces.m
+++ faces.m
@@ -43,11 +43,9 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 % theImage = webread(angry_url);
 % imagesc(theImage);
 
-imds = imageDatastore('/Users/shanzehsayied/Documents/Github/midterm-project/face-directory/*.png');
-imgs = readall(imds);
-theImage = imgs{1}
-imshow(theImage);
-
+url = 'https://i.ibb.co/H4R03hy/happy.png';
+happyimg = webread(url);
+imagesc(happyimg)
 %theImageLocation = [PsychtoolboxRoot 'PsychDemos' filesep...
 %    'AlphaImageDemo' filesep 'konijntjes1024x768.jpg'];
 %theImage = imread(theImageLocation);
@@ -56,7 +54,7 @@ imshow(theImage);
 
 
 % Get the size of the image
-[s1, s2, s3] = size(theImage);
+[s1, s2, s3] = size(happyimg);
 
 % Here we check if the image is too big to fit on the screen and abort if
 % it is. See ImageRescaleDemo to see how to rescale an image.
@@ -67,7 +65,7 @@ imshow(theImage);
 % end
 
 % Make the image into a texture
-imageTexture = Screen('MakeTexture', window, theImage);
+imageTexture = Screen('MakeTexture', window, happyimg);
 
 % Draw the image to the screen, unless otherwise specified PTB will draw
 % the texture full size in the center of the screen. We first draw the
