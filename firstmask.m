073bf3fbfa63538a145e25b652220ac0010740df
tpan2 <101124218+tpan2@users.noreply.github.com>
2022-03-18 21:04:01 -0400

    Create firstmask.m

---
+++ firstmask.m
@@ -0,0 +1,22 @@
+sca;
+close all;
+clear;
+
+% Here we call some default settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
+
+% Get the screen numbers
+screens = Screen('Screens');
+
+% Draw to the external screen if avaliable
+screenNumber = max(screens);
+
+% Define black and white
+white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
+
+% Open an on screen window
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, white);
+
+% Get the size of the on screen window
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
