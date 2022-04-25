5bc8ad185e9207e692658029a4c86465d0b79764
shanzehs <101124276+shanzehs@users.noreply.github.com>
2022-04-25 11:13:22 -0400

    Create mask_attempt_SS.m

---
+++ mask_attempt_SS.m
@@ -0,0 +1,23 @@
+MyImages = dir(fullfile(pwd,'*1.png'));
+RandomNumber = randi([1 size(MyImages,1)]);
+MyRandomImage = MyImages(RandomNumber).name;
+image = imread(MyRandomImage);
+% imshow(image);
+
+[rows, columns] = size(image);
+
+%mask = randi([0 1], size(image));
+%image(logical(image)) = mask(logical(image));
+%image(logical(image)) = rand(1, nnz(image));
+
+%image(image == 0) = randn(1, size(image(image == 0), 1));
+
+n = 0;
+while n < 1;
+    n = n+1;
+    mask = randi([0 1], size(image));
+    mask = uint8(mask);
+    actual_mask = mask.*image;
+end
+
+imshow(actual_mask)
