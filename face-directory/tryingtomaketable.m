07466deb68b87c449fb0221d50f6ea748e5680ca
tpan2 <101124218+tpan2@users.noreply.github.com>
2022-05-07 11:15:45 -0400

    Some updates to stats, merging sample test to test stats script. Still need to make normality, variance, and t-test

--- face-directory/tryingtomaketable.m
+++ face-directory/tryingtomaketable.m
@@ -7,3 +7,36 @@ for kk = 1:numSheets;
     t1 = readtable('Output.xlsx', 'Sheet',kk)
     t2 = [t2;t1]
 end
+
+data_matrix=table2array(t2);
+
+angry_data = [];
+happy_data = [];
+for ii = 1:size(data_matrix, 1)
+    % going across the rows in the data matrix
+    if data_matrix(ii, 1) == 0
+        angry_data(ii,:)= data_matrix(ii, end)
+        %  wherever the condition is 0 in the first column, angry_data will
+        %  be populated with the preference values
+    end
+    if data_matrix(ii, 1) == 1
+        happy_data(ii, :) = data_matrix(ii, end)
+        % same as above, just for happy data
+    end
+end
+angry_data = nonzeros(angry_data)
+happy_data = nonzeros(happy_data)
+%reorganizes all the data into columns and gets rid of zeroes
+
+mean(happy_data);
+mean(angry_data);
+std(happy_data);
+std(angry_data);
+
+ttest(angry_data, happy_data)
+
+%D'Agostino test to determine normality
+    %if not normal distr on both sets > Mann Whitney u-test
+    %if normal, 
+        %if equal variance > student t test
+        %if unequal variance > student t-test with Welch's correction
