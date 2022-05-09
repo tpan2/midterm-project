0e3048c97be90a85e50a26b0f404ccef3ed0bd4b
tpan2 <101124218+tpan2@users.noreply.github.com>
2022-05-09 12:53:41 -0400

    Moving the generation of the mask before the for loop

--- face-directory/compiled_script.m
+++ face-directory/compiled_script.m
@@ -56,6 +56,24 @@ function compiled_script()
     %Create a matrix for nfiles and 2 columns (one for condition and one for
     %valence)
     
+    %generate mask in beginning
+    MyImages = dir(fullfile(pwd,'*1.png'));
+        RandomNumber = randi([1 size(MyImages,1)]);
+        MyRandomImage = MyImages(RandomNumber).name;
+        image = imread(MyRandomImage);
+        
+    n = 0;
+        while n < 1;
+            n = n+1;
+            mask = ones(size(image));
+            noise_mask = randi([0 1], size(image));
+            mask = uint8(mask);
+            actual_mask = mask.*image;
+            nonzeroes = find(actual_mask(:,:,:) > 0);
+            mask(nonzeroes) = noise_mask(nonzeroes)*255;
+            final_mask = rgb2gray(mask);
+        end
+       
     for ii = 1:nfiles
         % For loop so that the experiment will run however many times as however
         % many salient faces we have in our directory.
@@ -111,22 +129,7 @@ function compiled_script()
         %filename = imagefile.name;
         %maskimage = imread(filename);
     
-        MyImages = dir(fullfile(pwd,'*1.png'));
-        RandomNumber = randi([1 size(MyImages,1)]);
-        MyRandomImage = MyImages(RandomNumber).name;
-        image = imread(MyRandomImage);
-    
-        n = 0;
-        while n < 1;
-            n = n+1;
-            mask = ones(size(image));
-            noise_mask = randi([0 1], size(image));
-            mask = uint8(mask);
-            actual_mask = mask.*image;
-            nonzeroes = find(actual_mask(:,:,:) > 0);
-            mask(nonzeroes) = noise_mask(nonzeroes)*255;
-            final_mask = rgb2gray(mask);
-        end
+       %placeholder for mask image generation
     
         % Make the image into a texture
         imageTexture = Screen('MakeTexture', window, final_mask);
