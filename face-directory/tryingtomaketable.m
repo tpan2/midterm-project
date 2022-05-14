d2fb542fae05d13916abaa495d4366b6c988c8ce
tpan2 <101124218+tpan2@users.noreply.github.com>
2022-05-14 09:02:52 -0400

    Generate summary statistics

--- face-directory/tryingtomaketable.m
+++ face-directory/tryingtomaketable.m
@@ -8,6 +8,13 @@ for kk = 1:numSheets;
     t2 = [t2;t1];
 end
 
+t2.Properties.VariableNames = ["condition","happinessdata"];
+%change names of columns of table
+
+%generate summary statistics
+stat1 = grpstats(t2, "condition",["mean", "std","sem","var","min","max"])
+
+%convert table to array
 data_matrix=table2array(t2);
 condition = table2array(t2(:,1));
 happinessdata = table2array(t2(:,2));
@@ -30,26 +37,21 @@ angry_data = nonzeros(angry_data);
 happy_data = nonzeros(happy_data);
 %reorganizes all the data into columns and gets rid of zeroes
 
-% mean(happy_data);
-% mean(angry_data);
-% std(happy_data);
-% std(angry_data);
-
 normality_angry = adtest(angry_data);
 normality_happy = adtest(happy_data);
 %Anderson-Darling test to determine normality
   if (normality_angry == 0) && (normality_happy ==0);  
       %if normal > student t test
-      [h,ttest_significance] = ttest(angry_data, happy_data);
+      [h,ttest_significance, ci, stats] = ttest(angry_data, happy_data)
       disp(['ttest_significance is ', num2str(ttest_significance)]);
   else;
       %if not normal distr on both sets > Mann Whitney u-test
-      [h,mww_significance]= ranksum(angry_data, happy_data);
+      [h,mww_significance, ci, stats]= ranksum(angry_data, happy_data);
       disp(['mww_significance is ', num2str(mww_significance)]);
   end
 
-
+%make simple boxplot to visualize data
 boxplot(happinessdata, condition)
-xlabel('Condition')
+xlabel('Condition (0 = Angry, 1 = Happy)')
 ylabel('Happiness Rating')
 title('Happiness Ratings by condition')
